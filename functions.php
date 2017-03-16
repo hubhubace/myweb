0e03947f2b8b6480911d44c58682261fb206ee18
Thompson Ike <lorenzo2022002@yahoo.com>
2017-03-16 12:58:06 +0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,10 +1,10 @@
 <?php
   function insertPost() {
    
-    if(isset($_POST["send"])) {
+    if(isset($_POST["c"])) {
      global $db_conx;
      global $log_id;
-      $content = $_POST['content']);
+      $c = $_POST['c']);
       
       $insert = "INSERT INTO fb_post (user_id,post_content,date_created) VALUES ('$log_id','$content', NOW())";
       
